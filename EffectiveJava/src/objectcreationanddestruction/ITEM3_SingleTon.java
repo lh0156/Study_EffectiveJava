90f753b9259e4993a33b693ddabf170ab3608cfb
Eom Yunseop <lh0156@naver.com>
2023-02-24 12:35:42 +0900

    update: ITEM3_SingleTon
    자바 객체를 싱글턴으로 만드는 방법
    1. static final 인스턴스를 만들어 public으로 열어둔다
    2. static final 인스턴스를 private로 닫아두고 public static으로 getInstance 메소드를 생성한다.
    3. 원소가 하나인 enum 타입을 선언한다.

---
+++ EffectiveJava/src/objectcreationanddestruction/ITEM3_SingleTon.java
@@ -0,0 +1,31 @@
+package objectcreationanddestruction;
+
+public class ITEM3_SingleTon {
+
+    public static final ITEM3_SingleTon INSTANCE = new ITEM3_SingleTon();
+
+    private ITEM3_SingleTon() {
+        //...
+    }
+
+    private static final ITEM3_SingleTon INSTANCE2 = new ITEM3_SingleTon();
+
+    // 방법 2
+    public static ITEM3_SingleTon getInstance() {
+        return INSTANCE2;
+    }
+
+    // 가짜 ITEM3_SingleTon 생성을 예방해주는 코드
+    private Object readResolve() {
+        // '진짜' ITEM3_SingleTon을 반환하고, 가짜 ITEM_SingleTon은 가비지 컬렉터에 맡긴다.
+        return INSTANCE2;
+    }
+}
+
+enum ITEM3_SingleTon2 {
+    INSTANCE;
+
+    public void leaveTheBuilding() {
+
+    }
+}
