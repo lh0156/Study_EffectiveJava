02ebea6f948bb05a2a5db8e4c2a80af47cd81517
Eom Yunseop <lh0156@naver.com>
2023-02-24 12:58:44 +0900

    update: ITEM4_NotInstantiate

---
+++ EffectiveJava/src/objectcreationanddestruction/ITEM4_NotInstantiate.java
@@ -0,0 +1,14 @@
+package objectcreationanddestruction;
+
+public class ITEM4_NotInstantiate {
+
+    private ITEM4_NotInstantiate() {
+
+    }
+    
+    static String var1 = "var1";
+    static String var2 = "var2";
+    static String var3 = "var3";
+    static String var4 = "var4";
+    static String var5 = "var5";
+}
