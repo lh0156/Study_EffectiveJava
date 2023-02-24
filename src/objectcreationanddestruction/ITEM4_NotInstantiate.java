c03ed913cd60f3505b3e89d7474d1c33f2983b10
Eom Yunseop <lh0156@naver.com>
2023-02-24 13:02:24 +0900

    form update

--- src/objectcreationanddestruction/ITEM4_NotInstantiate.java
+++ src/objectcreationanddestruction/ITEM4_NotInstantiate.java
@@ -5,7 +5,7 @@ public class ITEM4_NotInstantiate {
     private ITEM4_NotInstantiate() {
 
     }
-    
+
     static String var1 = "var1";
     static String var2 = "var2";
     static String var3 = "var3";
