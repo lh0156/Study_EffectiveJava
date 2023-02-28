a686de8f84038b32966b32c976255f508310d040
Eom Yunseop <lh0156@naver.com>
2023-02-28 10:24:54 +0900

    update: abstract class test  * abstract is can't prevent inheritance

---
+++ src/objectcreationanddestruction/item4_not_instantiate/ITEM4_AbstractInstantiate.java
@@ -0,0 +1,9 @@
+package objectcreationanddestruction.item4_not_instantiate;
+
+public abstract class ITEM4_AbstractInstantiate {
+
+    static final String var = "var";
+    static final String var1 = "var1";
+    static final String var2 = "var2";
+
+}
---
+++ src/objectcreationanddestruction/item4_not_instantiate/ITEM4_ExtendClass.java
@@ -0,0 +1,8 @@
+package objectcreationanddestruction.item4_not_instantiate;
+
+public class ITEM4_ExtendClass extends ITEM4_AbstractInstantiate{
+
+    ITEM4_AbstractInstantiate item4_abstractInstantiate = new ITEM4_ExtendClass();
+
+
+}
