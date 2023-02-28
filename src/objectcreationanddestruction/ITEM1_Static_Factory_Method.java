38a8262286ac3fd13f94b38c95b33442e85d7249
Eom Yunseop <lh0156@naver.com>
2023-02-28 09:48:37 +0900

    update: ITEM1_Static_Factory_Method

---
+++ src/objectcreationanddestruction/ITEM1_Static_Factory_Method.java
@@ -0,0 +1,9 @@
+package objectcreationanddestruction;
+
+public class ITEM1_Static_Factory_Method {
+
+    public static Boolean valueOf(boolean b) {
+        return b ? Boolean.TRUE : Boolean.FALSE;
+    }
+
+}
