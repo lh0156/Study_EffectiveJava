966902960a54d0d1aab29a33c82f399b4f3a1c36
Eom Yunseop <lh0156@naver.com>
2023-03-08 13:06:00 +0900

    add Item 10

---
+++ src/common_methods_of_all_objects/item10_equals/CaseInsensitiveString.java
@@ -0,0 +1,21 @@
+package common_methods_of_all_objects.item10_equals;
+
+public class CaseInsensitiveString {
+
+//    private final String s;
+
+//    public CaseInsensitiveString(String s) {
+//        this.s = Object.requireNotNull(s);
+//    }
+
+//    @Override
+//    public boolean equals(Object o) {
+//
+//        if (o instanceof CaseInsensitiveString)
+//            return s.equalsIgnoreCase(((CaseInsensitiveString) o).s);
+//        if (o instanceof String)
+//            return s.equalsIgnoreCase((String) o);
+//        return false;
+//
+//    }
+}
---
+++ src/common_methods_of_all_objects/item10_equals/ITEM10_Equals.java
@@ -0,0 +1,11 @@
+package common_methods_of_all_objects.item10_equals;
+
+public class ITEM10_Equals {
+
+    //호출 금지
+    @Override
+    public boolean equals(Object o) {
+        throw new AssertionError();
+    }
+
+}
