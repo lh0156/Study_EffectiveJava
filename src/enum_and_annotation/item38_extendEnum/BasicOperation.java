26080d2316eb5d7439a5848fbd201ce87cca7b5f
yseom <lh0156@naver.com>
2023-04-21 08:59:59 +0900

    fetch: item38 extendEnum

---
+++ src/enum_and_annotation/item38_extendEnum/BasicOperation.java
@@ -0,0 +1,25 @@
+package enum_and_annotation.item38_extendEnum;
+
+public enum BasicOperation implements Operation {
+
+    PLUS("+") {
+        public double apply(double x, double y) { return x + y; }
+    },
+    MINUS("-") {
+        public double apply(double x, double y) { return x - y; }
+    };
+
+    private final String symbol;
+
+    BasicOperation(String symbol) {
+        this.symbol = symbol;
+    }
+
+    @Override
+    public String toString() {
+        return symbol;
+    }
+
+
+
+}
---
+++ src/enum_and_annotation/item38_extendEnum/Operation.java
@@ -0,0 +1,6 @@
+package enum_and_annotation.item38_extendEnum;
+
+public interface Operation {
+    double apply(double x, double y);
+
+}
